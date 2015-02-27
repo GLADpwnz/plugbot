87a85fec6a206bab2beed8c840036ac7088fd828
NikallassZadske <nikallasszadske@gmail.com>
2015-02-27 23:27:42 +0500

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -54,7 +54,7 @@
     var loadChat = function (cb) {
         if (!cb) cb = function () {
         };
-        $.get("https://raw.githubusercontent.com/NikallassZadske/basicBot/master/lang/ru.json", function (json) {
+        $.get("https://rawgit.com/NikallassZadske/basicBot/master/lang/langIndex.json", function (json) {
             var link = basicBot.chatLink;
             if (json !== null && typeof json !== "undefined") {
                 langIndex = json;
